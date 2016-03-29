78cec5711a1cce0fcb9db53f567355d64e10de64
Adrien Cadet <acadet@live.fr>
2016-03-29 15:22:22 -0700

    Creates route for listing pictures related to a place

---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,21 @@
+class PicturesController < ApplicationController
+  def for_place
+    place_id = params[:id]
+    p        = Place.all.select { |e| e.id == place_id }.first
+    status   = if p.nil? then
+                 :not_found
+               else
+                 :ok
+               end
+    data     = if p.nil? then
+                 {}
+               else
+                 p.pictures
+               end
+
+    respond_to do |f|
+      f.html
+      f.json { render json: data, status: status }
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,11 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'places#index'
 
-  resources :places, only: [:index]
+  resources :places, only: [:index] do
+    member do
+      get 'pictures' => 'pictures#for_place'
+    end
+  end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
