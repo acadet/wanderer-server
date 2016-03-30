2ba018476bf11976db48a697784d93d708d6f913
Adrien Cadet <acadet@live.fr>
2016-03-29 17:45:11 -0700

    Removes CSRF protection for JSON request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # Avoid JSON requests to be revoked
+  skip_before_filter :verify_authenticity_token, if: :json_request?
+
+  protected
+
+  def json_request?
+    request.format.json?
+  end
 end
