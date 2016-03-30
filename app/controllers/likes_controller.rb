ecf73a76931b32c963200eef1b33508f4cc35469
Adrien Cadet <acadet@live.fr>
2016-03-29 17:44:54 -0700

    Fixes like toggle

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,8 @@
 class LikesController < ApplicationController
   def toggle
+    puts params
     p = params.require(:like).permit(:device_token)
-    Like.toggle(params[:id], p[:device_token])
+    Like.toggle(params[:id].to_i, p[:device_token])
 
     respond_to do |f|
       f.html
