b228228be1d83930be45a3370d242b97b536144b
Adrien Cadet <acadet@live.fr>
2016-04-05 10:44:54 -0700

    First draft of original_data

---
+++ original_data.rb
@@ -0,0 +1,308 @@
+pictures = [
+    {
+        id:       1,
+        url:      "https://raw.githubusercontent.com/acadet/wanderer-stock/master/big-sur-1.jpg",
+        place_id: 1
+    },
+    {
+        id:       2,
+        url:      "https://raw.githubusercontent.com/acadet/wanderer-stock/master/big-sur-2.jpeg",
+        place_id: 1
+    },
+    {
+        id:       3,
+        url:      "https://raw.githubusercontent.com/acadet/wanderer-stock/master/la-1.jpeg",
+        place_id: 2
+    },
+    {
+        id:       4,
+        url:      "https://raw.githubusercontent.com/acadet/wanderer-stock/master/la-2.jpeg",
+        place_id: 2
+    },
+    {
+        id:       5,
+        url:      "https://raw.githubusercontent.com/acadet/wanderer-stock/master/la-3.jpeg",
+        place_id: 2
+    },
+    {
+        id:       6,
+        url:      "https://raw.githubusercontent.com/acadet/wanderer-stock/master/nyc-1.jpg",
+        place_id: 3
+    },
+    {
+        id:       7,
+        url:      "https://raw.githubusercontent.com/acadet/wanderer-stock/master/nyc-2.jpg",
+        place_id: 3
+    },
+    {
+        id:       8,
+        url:      "https://raw.githubusercontent.com/acadet/wanderer-stock/master/nyc-3.jpg",
+        place_id: 3
+    },
+    {
+        id:       9,
+        url:      "https://raw.githubusercontent.com/acadet/wanderer-stock/master/chicago-1.jpg",
+        place_id: 4
+    },
+    {
+        id:       10,
+        url:      "https://raw.githubusercontent.com/acadet/wanderer-stock/master/grand-canyon-1.jpg",
+        place_id: 5
+    },
+    {
+        id:       11,
+        url:      "https://raw.githubusercontent.com/acadet/wanderer-stock/master/grand-canyon-2.jpg",
+        place_id: 5
+    },
+    {
+        id:       12,
+        url:      "https://raw.githubusercontent.com/acadet/wanderer-stock/master/grand-canyon-3.jpg",
+        place_id: 5
+    },
+    {
+        id:       13,
+        url:      "https://raw.githubusercontent.com/acadet/wanderer-stock/master/grand-canyon-4.jpg",
+        place_id: 5
+    },
+    {
+        id:       14,
+        url:      "https://raw.githubusercontent.com/acadet/wanderer-stock/master/monument-valley-1.jpg",
+        place_id: 6
+    },
+    {
+        id:       15,
+        url:      "https://raw.githubusercontent.com/acadet/wanderer-stock/master/bryce-canyon-1.jpg",
+        place_id: 7
+    },
+    {
+        id:       16,
+        url:      "https://raw.githubusercontent.com/acadet/wanderer-stock/master/bryce-canyon-2.jpg",
+        place_id: 7
+    },
+    {
+        id:       17,
+        url:      "https://raw.githubusercontent.com/acadet/wanderer-stock/master/lake-powell-1.jpg",
+        place_id: 8
+    },
+    {
+        id:       18,
+        url:      "https://raw.githubusercontent.com/acadet/wanderer-stock/master/death-valley-1.jpg",
+        place_id: 9
+    },
+    {
+        id:       19,
+        url:      "https://raw.githubusercontent.com/acadet/wanderer-stock/master/death-valley-2.jpg",
+        place_id: 9
+    },
+    {
+        id:       20,
+        url:      "https://raw.githubusercontent.com/acadet/wanderer-stock/master/yosemite-1.jpg",
+        place_id: 10
+    },
+    {
+        id:       21,
+        url:      "https://raw.githubusercontent.com/acadet/wanderer-stock/master/yosemite-2.jpg",
+        place_id: 10
+    },
+    {
+        id:       22,
+        url:      "https://raw.githubusercontent.com/acadet/wanderer-stock/master/yosemite-3.jpg",
+        place_id: 10
+    },
+    {
+        id:       23,
+        url:      "https://raw.githubusercontent.com/acadet/wanderer-stock/master/emigrant-wilderness-1.jpg",
+        place_id: 11
+    },
+    {
+        id:       24,
+        url:      "https://raw.githubusercontent.com/acadet/wanderer-stock/master/emigrant-wilderness-2.jpg",
+        place_id: 11
+    },
+    {
+        id:       25,
+        url:      "https://raw.githubusercontent.com/acadet/wanderer-stock/master/london-1.jpg",
+        place_id: 12
+    },
+    {
+        id:       26,
+        url:      "https://raw.githubusercontent.com/acadet/wanderer-stock/master/london-2.jpg",
+        place_id: 12
+    },
+    {
+        id:       27,
+        url:      "https://raw.githubusercontent.com/acadet/wanderer-stock/master/london-3.jpg",
+        place_id: 12
+    },
+    {
+        id:       28,
+        url:      "https://raw.githubusercontent.com/acadet/wanderer-stock/master/dublin-1.jpg",
+        place_id: 13
+    },
+    {
+        id:       29,
+        url:      "https://raw.githubusercontent.com/acadet/wanderer-stock/master/dublin-2.jpg",
+        place_id: 13
+    },
+    {
+        id:       30,
+        url:      "https://raw.githubusercontent.com/acadet/wanderer-stock/master/barcelona-1.jpg",
+        place_id: 14
+    },
+    {
+        id:       31,
+        url:      "https://raw.githubusercontent.com/acadet/wanderer-stock/master/barcelona-2.jpg",
+        place_id: 14
+    },
+    {
+        id:       32,
+        url:      "https://raw.githubusercontent.com/acadet/wanderer-stock/master/barcelona-3.jpg",
+        place_id: 14
+    },
+]
+
+places = [
+    {
+        id:              1,
+        name:            "Big Sur",
+        country:         "USA",
+        latitude:        36.270421,
+        longitude:       -121.807976,
+        description:     "",
+        visit_date:      "2015-08-01",
+        main_picture_id: 1
+    },
+    {
+        id:              2,
+        name:            "Los Angeles",
+        country:         "USA",
+        latitude:        34.052234,
+        longitude:       -118.243685,
+        description:     "",
+        visit_date:      "2015-08-01",
+        main_picture_id: 3
+    },
+    {
+        id:              3,
+        name:            "NYC",
+        country:         "USA",
+        latitude:        40.712784,
+        longitude:       -74.005941,
+        description:     "",
+        visit_date:      "2015-04-01",
+        main_picture_id: 6
+    },
+    {
+        id:              4,
+        name:            "Chicago",
+        country:         "USA",
+        latitude:        41.878114,
+        longitude:       -87.629798,
+        description:     "",
+        visit_date:      "2015-04-10",
+        main_picture_id: 9
+    },
+    {
+        id:              5,
+        name:            "Grand Canyon",
+        country:         "USA",
+        latitude:        36.054445,
+        longitude:       -112.140111,
+        description:     "",
+        visit_date:      "2015-05-01",
+        main_picture_id: 10
+    },
+    {
+        id:              6,
+        name:            "Monument Valley",
+        country:         "USA",
+        latitude:        37.004245,
+        longitude:       -110.173478,
+        description:     "",
+        visit_date:      "2015-05-02",
+        main_picture_id: 14
+    },
+    {
+        id:              7,
+        name:            "Bryce Canyon",
+        country:         "USA",
+        latitude:        37.628316,
+        longitude:       -112.167695,
+        description:     "",
+        visit_date:      "2015-05-04",
+        main_picture_id: 15
+    },
+    {
+        id:              8,
+        name:            "Lake Powell",
+        country:         "USA",
+        latitude:        37.068262,
+        longitude:       -111.243311,
+        description:     "",
+        visit_date:      "2015-05-03",
+        main_picture_id: 17
+    },
+    {
+        id:              9,
+        name:            "Death Valley",
+        country:         "USA",
+        latitude:        36.532265,
+        longitude:       -116.932541,
+        description:     "",
+        visit_date:      "2015-05-05",
+        main_picture_id: 18
+    },
+    {
+        id:              10,
+        name:            "Yosemite",
+        country:         "USA",
+        latitude:        37.865101,
+        longitude:       -119.538329,
+        description:     "",
+        visit_date:      "2015-05-06",
+        main_picture_id: 20
+    },
+    {
+        id:              11,
+        name:            "Emigrant Wilderness",
+        country:         "USA",
+        latitude:        38.2917988,
+        longitude:       -120.6855901,
+        description:     "",
+        visit_date:      "2015-07-01",
+        main_picture_id: 23
+    },
+    {
+        id:              12,
+        name:            "London",
+        country:         "UK",
+        latitude:        51.507351,
+        longitude:       -0.127758,
+        description:     "",
+        visit_date:      "2013-06-01",
+        main_picture_id: 25
+    },
+    {
+        id:              13,
+        name:            "Dublin",
+        country:         "Ireland",
+        latitude:        53.349805,
+        longitude:       -6.260310,
+        description:     "",
+        visit_date:      "2013-05-01",
+        main_picture_id: 28
+    },
+    {
+        id:              14,
+        name:            "Barcelona",
+        country:         "Spain",
+        latitude:        41.385064,
+        longitude:       2.173403,
+        description:     "",
+        visit_date:      "2014-08-01",
+        main_picture_id: 30
+    }
+]
+
+places.each { |e| Place.new(e).save }
+pictures.each { |e| Picture.new(e).save }
