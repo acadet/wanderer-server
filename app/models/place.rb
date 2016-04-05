cb744359d20f2e7e77ce78f2e70c8a7e56bc315d
Adrien Cadet <acadet@live.fr>
2016-04-05 13:28:58 -0700

    Changes relationship type for main picture

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,7 @@
 class Place < ActiveRecord::Base
   has_many :pictures
   has_many :likes
-  has_one :main_picture, class_name: "Picture", foreign_key: :id
+  belongs_to :main_picture, class_name: "Picture"
 
   scope :by_visit_date, -> { order(visit_date: :desc) }
 
